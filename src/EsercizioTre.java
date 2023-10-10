2375cd9b77dcd7fa2f6d33bf314d5e047b185c95
MrMax01 <massimo.mauro00@gmail.com>
2023-10-10 16:57:01 +0200

    have to fix

---
+++ src/EsercizioTre.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class EsercizioTre {
+    public static void main(String[] args) {
+        Map<String, Integer> addressBook= new HashMap<>();
+        addressBook.put("Massimo", 346785166);
+        addressBook.put("Andrea",534782879);
+        addressBook.put("Jack", 438743809);
+        System.out.println(addressBook);
+        addressBook.remove("Massimo");
+        System.out.println(addressBook);
+        findFromValue(addressBook, 346785166);
+
+    }
+
+    public static void findFromValue (Map myMap, int value){
+        Set<String> chiavi = myMap.keySet();
+        String keyResult="";
+        for (String chiave : chiavi) {
+          if(myMap.get(chiave)==value) keyResult= chiave;
+        //da risolvere un atitmo
+        }
+        System.out.println(myMap.get(keyResult));
+    }
+}
